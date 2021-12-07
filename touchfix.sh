e1b02d6dbc7394d5f435e144d2086131f01da1e8
Lars Birkeland <30419055+Lasobi@users.noreply.github.com>
2021-12-07 13:27:49 +0000

    Create touchfix.sh

---
+++ touchfix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xinput set-prop 'ILITEK ILITEK-TP' --type=float 'Coordinate Transformation Matrix' 0.5 0 0 0 1 0 0 0 1
+xinput set-prop 'ELAN0732:00 04F3:2650' --type=float 'Coordinate Transformation Matrix' 0.5 0 0.5 0 1 0 0 0 1
